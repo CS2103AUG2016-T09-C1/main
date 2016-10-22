eabd3e1a0a504d3fd4602c8faae236cdcb1eadb0
Ashley Ong <ashley_2461994@hotmail.com>
2016-10-22 14:45:15 +0800

    Change version number

--- src/main/java/seedu/inbx0/MainApp.java
+++ src/main/java/seedu/inbx0/MainApp.java
@@ -31,7 +31,7 @@ import java.util.logging.Logger;
 public class MainApp extends Application {
     private static final Logger logger = LogsCenter.getLogger(MainApp.class);
 
-    public static final Version VERSION = new Version(1, 0, 0, true);
+    public static final Version VERSION = new Version(0, 4, 0, true);
 
     protected Ui ui;
     protected Logic logic;
