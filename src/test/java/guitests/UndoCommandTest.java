318b5c5dd5069aefb4e44ff7ba5a4cf8cb082337
Ashley Ong <ashley_2461994@hotmail.com>
2016-10-28 15:34:10 +0800

    Add tests for UndoCommand

--- src/test/java/guitests/UndoCommandTest.java
+++ src/test/java/guitests/UndoCommandTest.java
@@ -7,10 +7,11 @@ import static org.junit.Assert.assertTrue;
 import seedu.inbx0.commons.exceptions.IllegalValueException;
 import seedu.inbx0.logic.commands.UndoCommand;
 import seedu.inbx0.testutil.TestTask;
+import seedu.inbx0.testutil.TestUtil;
 
+//@@author A0139481Y
 public class UndoCommandTest extends TaskListGuiTest {
     
-    //@@author A0139481Y
     @Test
     public void undoZeroCommand() throws IllegalArgumentException, IllegalValueException {
         TestTask[] currentList = td.getTypicalTasks();
@@ -36,6 +37,36 @@ public class UndoCommandTest extends TaskListGuiTest {
         commandBox.runCommand("del 1");
         commandBox.runCommand("del 1");
         assertUndoResult("undo 2", UndoCommand.MESSAGE_UNDO_TASK_SUCCESS, currentList);
+    }
+    
+    @Test
+    public void undoOneDelCommandsFromTwo() throws IllegalArgumentException, IllegalValueException {
+        TestTask[] currentList = td.getTypicalTasks();
+        commandBox.runCommand("del 1");
+        commandBox.runCommand("del 1");
+        TestTask[] expectedRemainder = TestUtil.removeTaskFromList(currentList, 1);
+        assertUndoResult("undo", UndoCommand.MESSAGE_UNDO_TASK_SUCCESS, expectedRemainder);
+    }
+    
+    @Test
+    public void undoInvalidInput() throws IllegalArgumentException, IllegalValueException {
+        TestTask[] currentList = td.getTypicalTasks();
+        commandBox.runCommand("del 1");
+        TestTask[] expectedRemainder = TestUtil.removeTaskFromList(currentList, 1);
+        commandBox.runCommand("del 1");
+        expectedRemainder = TestUtil.removeTaskFromList(expectedRemainder, 1);
+        assertUndoResult("undo -2", "Invalid command format! \n" + UndoCommand.MESSAGE_USAGE, expectedRemainder);
+        assertUndoResult("undo String", "Invalid command format! \n" + UndoCommand.MESSAGE_USAGE, expectedRemainder);
+    }
+    
+    @Test
+    public void undoAllCommands() throws IllegalArgumentException, IllegalValueException {
+        TestTask[] currentList = td.getTypicalTasks();
+        commandBox.runCommand("del 1");
+        TestTask[] remainder = TestUtil.removeTaskFromList(currentList, 1);
+        commandBox.runCommand("del 1");
+        remainder = TestUtil.removeTaskFromList(remainder, 1);
+        assertUndoResult("undo 4", UndoCommand.MESSAGE_UNDO_TASK_SUCCESS, currentList);
     }
     
     private void assertUndoResult(String command, String expectedMessage, TestTask... currentList) 
